27cff50b85c9bd79a8ff56eb103b5416a3c43451
Kristina <152077588+Kristina-Af@users.noreply.github.com>
2023-12-19 00:21:38 +0300

    Update ISort.h
    комментирование некоторых функций

--- ISort.h
+++ ISort.h
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-class ISort {
+class ISort {  //базовый класс
 protected:
 public:
 	virtual void Sort(int** array, int strings, int columns) = 0;
-	virtual ~ISort() = default;
+	virtual ~ISort() = default; //virtual - используется для освобождения ресурсов, используемых объектом производного класса, когда он удаляется из памяти.
 };
 
-class InsertSort : public ISort {
+class InsertSort : public ISort { //производный класс
 public:
 	void Sort(int** array, int strings, int columns) override;
 };
