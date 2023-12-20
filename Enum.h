602954d151bdc1e8789b4ad5a56cc9dc34463513
Kristina <tina770904@gmail.com>
2023-12-21 02:38:37 +0300

    создание нового файла перечисления и приравнивание первого значения к 1

---
+++ Enum.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum PrintMetod { StartProgramm = 1,
+	StartModuleTests,
+	StopProgramm };
+
+enum InputMethod { FromFile = 1, 
+	ManualMethod, 
+	RandomMethod,
+	ReturnMenu };
+
+enum CreatingAFile { CreateFile = 1, 
+	ContunueWithoutCreatingFile };
--- MenuInterface.h
+++ MenuInterface.h
@@ -1,11 +1,7 @@
 #pragma once
 #include "ISort.h"
-
+#include "Enum.h"
 using namespace std;
-
-enum PrintMetod { StartProgramm = 1, StartModuleTests = 2, StopProgramm = 3 };
-enum InputMethod { FromFile = 1, ManualMethod = 2, RandomMethod = 3, ReturnMenu = 4 };
-enum CreatingAFile { CreateFile = 1, ContunueWithoutCreatingFile = 2 };
 
 void ShowGreeting();
 void InputMethodMenu();
