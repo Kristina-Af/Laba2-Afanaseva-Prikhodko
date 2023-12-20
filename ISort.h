23e7156b5f9cdc8f927d650df52d1690ed924b7c
Kristina <tina770904@gmail.com>
2023-12-21 01:55:43 +0300

    комментирование функций в ISort.h

--- ISort.h
+++ ISort.h
@@ -6,11 +6,23 @@ using namespace std;
 class ISort {  //базовый класс
 protected:
 public:
+	/// <summary>
+	/// Сортировка вставками
+	/// </summary>
+	/// <param name="array"></param>
+	/// <param name="strings"></param>
+	/// <param name="columns"></param>
 	virtual void Sort(int** array, int strings, int columns) = 0;
 	virtual ~ISort() = default; //virtual - используется для освобождения ресурсов, используемых объектом производного класса, когда он удаляется из памяти.
 };
 
 class InsertSort : public ISort { //производный класс
 public:
+	/// <summary>
+	/// Сортировка вставками
+	/// </summary>
+	/// <param name="array"></param>
+	/// <param name="strings"></param>
+	/// <param name="columns"></param>
 	void Sort(int** array, int strings, int columns) override;
 };
