d12b027e85fc564b574aad2a6740678bd2c752db
Kristina <tina770904@gmail.com>
2023-12-21 04:16:12 +0300

    редактирование переменных

--- MenuInterface.cpp
+++ MenuInterface.cpp
@@ -21,11 +21,11 @@ void InputMethodMenu() {
 	int rows = 0;
 	int columns = 0;
 	int input = 0;
-	int intTmp;
-	string temp;
+	int intTmp; //intTmp - используется для временного хранения считанного значения из файла
+	string line; //хранение строки при считывании из файла
 	string path;
-	string value;
-	int* tmp = nullptr; //nullptr - константа нулевого указателя
+	string value; // переменная, в которую считывается значение из файла
+	int* tmpIntArray = nullptr; //nullptr - константа нулевого указателя, для хранения целочисленного значения массива
 	int** array = nullptr;
 	ifstream file;
 	int choseMethod = 0;
@@ -38,18 +38,21 @@ void InputMethodMenu() {
 		case FromFile:
 			cout << "Введите путь файла:" << endl << "> ";
 			path = ReadStringWithoutWhitespace();
+
 			while (!FileExists(path)) {
 				cout << "Ошибка при открытии файла! Попробуйте ввести путь снова." << endl << "> ";
 				path = ReadStringWithoutWhitespace();
 			}
 			file.open(path);
 			cout << "Файл успешно открыт!" << endl;
+
 			if (file.peek() == ifstream::traits_type::eof()) { //traits_type - дает возможность проверять и преобразовывать свойства типов, eof - выдаёт истину, если файл пуст, а если файл не пуст, то выдаётся ложь
 				cout << "Файл пуст. Заполните файл и попробуйте еще раз." << endl;
 			}
 			else {
 				while (!file.eof()) {
 					file >> intTmp;
+
 					if (file.fail()) { //проверка ошибки при чтении файла
 						file.clear();
 						fileIsCorrect = false;
@@ -58,40 +61,50 @@ void InputMethodMenu() {
 				}
 				file.clear();
 				file.seekg(0);
+
 				while (!file.eof()) {
 					if ((input = file.get()) == '\n' || input == EOF) { rows += 1; } //eof - возвращает ненулевое значение
 				}
 				file.clear();
 				file.seekg(0); //file.seekg - позволяет искать произвольную позицию в файле.
-				tmp = new int[rows];
+				tmpIntArray = new int[rows];
+
 				for (int i = 0; i < rows; i++) {
-					tmp[i] = 0; 
-					getline(file, temp);
-					istringstream stream(temp);
+					tmpIntArray[i] = 0;
+					getline(file, line);
+					istringstream stream(line);
+
 					while (stream >> value) {
-						tmp[i]++;
+						tmpIntArray[i]++;
 					}
-					columns = tmp[i];
+					columns = tmpIntArray[i];
 				}
+
 				file.clear();
 				file.seekg(0);
+
 				for (int i = 0; i < rows - 1; i++) {
-					if (tmp[i] != tmp[i + 1]) {
+					if (tmpIntArray[i] != tmpIntArray[i + 1]) {
 						fileIsCorrect = false;
 					}
 				}
-				delete[] tmp;
-				tmp = nullptr;
+
+				delete[] tmpIntArray;
+				tmpIntArray = nullptr;
+
 				if (!fileIsCorrect) {
 					cout << "В файле содержится некорректно заполненная матрица. Исправьте файл и попробуйте еще раз." << endl;
 					file.close();
 					choseMethod = ReturnMenu;
 					break;
 				}
+
 				array = new int* [rows];
+
 				for (int i = 0; i < rows; i++) {
 					array[i] = new int[columns];
 				}
+
 				for (int i = 0; i < rows; i++) {
 					for (int j = 0; j < columns; j++) {
 						file >> array[i][j];
@@ -103,6 +116,7 @@ void InputMethodMenu() {
 			file.close();
 			choseMethod = ReturnMenu;
 			break;
+
 		case ManualMethod:
 			cout << "Введите размер массива: ";
 			rows = CheckInt();
@@ -126,6 +140,7 @@ void InputMethodMenu() {
 			CompleteControlWork(array, rows, columns);
 			choseMethod = ReturnMenu;
 			break;
+
 		case RandomMethod:
 			cout << "Введите количество строк массива: ";
 			rows = CheckInt();
@@ -153,8 +168,10 @@ void InputMethodMenu() {
 			CompleteControlWork(array, rows, columns);
 			choseMethod = ReturnMenu;
 			break;
+
 		case ReturnMenu:
 			break;
+
 		default:
 			cout << "Такого пункта нет в меню, попробуйте еще раз" << endl;
 			break;
@@ -166,9 +183,11 @@ void InputMethodMenu() {
 void CompleteControlWork(int** array, int rows, int columns) {
 	int** copyarray = nullptr; //передача двумерного массива в функцию
 	copyarray = new int* [rows];
+
 	for (int i = 0; i < rows; i++) {
 		copyarray[i] = new int[columns];
 	}
+
 	cout << endl << "Исходный массив:" << endl;
 	PrintArray(array, rows, columns);
 	cout << endl << "Сортировка вставками:" << endl;
@@ -183,24 +202,27 @@ void CompleteControlWork(int** array, int rows, int columns) {
 void StartProgram() {
 	setlocale(LC_ALL, "RU");
 	srand(static_cast<unsigned int>(time(NULL)));
-	int UserChoise = 0;
+	int userChoise = 0;
 	do {
 		system("cls");
 		ShowGreeting();
-		UserChoise = CheckInt();
-		switch (UserChoise) {
+		userChoise = CheckInt();
+		switch (userChoise) {
 		case StartProgramm:
 			InputMethodMenu();
 			break;
+
 		case StartModuleTests:
 			CompleteModuleTests();
 			break;
+
 		case StopProgramm:
 			break;
+
 		default:
 			cout << "Такого пункта нет в меню, попробуйте еще раз" << endl;
 			break;
 		}
-		if (UserChoise != StopProgramm)	system("pause");
-	} while (UserChoise != StopProgramm);
+		if (userChoise != StopProgramm)	system("pause");
+	} while (userChoise != StopProgramm);
 }
