7fd9c06e7c11ed9cb790b3562d9a1714148addbe
Kristina <152077588+Kristina-Af@users.noreply.github.com>
2024-01-19 20:16:55 +0300

    input.cpp

---
+++ Rps_lab3/input.cpp
@@ -0,0 +1,7 @@
+﻿#include "input.h"
+
+
+int getRandInt(int low_bound, int up_bound) {
+    // Генерация случайного числа в заданном диапазоне
+    return (rand() % (up_bound - low_bound) + low_bound);
+}
