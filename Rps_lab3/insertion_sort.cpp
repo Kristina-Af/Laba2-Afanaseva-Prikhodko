de80be02399d052380f57c72d2f557d7220003b7
alsssona <153617860+alsssona@users.noreply.github.com>
2024-01-19 20:20:45 +0300

    добавление сортировки

---
+++ Rps_lab3/insertion_sort.cpp
@@ -0,0 +1,17 @@
+﻿#include "insertion_sort.h"
+
+
+void InsertSort::sort(std::vector<int> &array) {
+    int tmp{}, // временная переменная для хранения значения элемента сортируемого массива
+        item{}; // индекс предыдущего элемента
+    for (int i = 0; i < array.size(); i++) {
+        tmp = array[i]; // инициализируем временную переменную текущим значением элемента массива
+        item = i - 1; // запоминаем индекс предыдущего элемента массива
+
+        while (item >= 0 && array[item] > tmp) {
+            array[item + 1] = array[item];
+            array[item] = tmp;
+            item--;
+        }
+    }
+}
---
+++ Rps_lab3/insertion_sort.h
@@ -0,0 +1,20 @@
+﻿#ifndef COMB_SORT_H
+#define COMB_SORT_H
+
+#include <vector>
+
+
+/// <summary>
+/// Класс, реализующий сортировку вставками.
+/// </summary>
+class InsertSort {
+public:
+    /// <summary>
+    /// Сортирует вектор с использованием сортировки вставками.
+    /// </summary>
+    /// <param name="array">Вектор, который нужно отсортировать.</param>
+    void sort(std::vector<int> &array);
+};
+
+
+#endif
